53b08ea4ec6491d0ae1745f178f3dbb09c3fdbf7
Kusuma6613 <93611002+Kusuma6613@users.noreply.github.com>
2022-02-01 22:27:51 +0530

    Add files via upload

---
+++ DFS.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+int a[20][20],reach[20],n;
+void dfs(int v) {
+	int i;
+	reach[v]=1;
+	for (i=1;i<=n;i++)
+	  if(a[v][i] && !reach[i]) {
+		printf("\n %d->%d",v,i);
+		dfs(i);
+	}
+}
+void main() {
+	int i,j,count=0;
+		printf("\n Enter number of vertices:");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++) {
+		reach[i]=0;
+		for (j=1;j<=n;j++)
+		   a[i][j]=0;
+	}
+	printf("\n Enter the adjacency matrix:\n");
+	for (i=1;i<=n;i++)
+	  for (j=1;j<=n;j++)
+	   scanf("%d",&a[i][j]);
+	dfs(1);
+	printf("\n");
+
+}
+//OUTPUT
+ Enter number of vertices:4
+
+ Enter the adjacency matrix:
+1 4 5 6
+2 9 0 12
+4 2 3 7
+9 14 0 22
+
+ 1->2
+ 2->4
+ 1->3
+
+Process returned 10 (0xA)   execution time : 32.478 s
+Press any key to continue.
+
